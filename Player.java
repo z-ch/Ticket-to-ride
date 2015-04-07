66ac908c6bb38b2a998c71d285142d7e8b12447b
Zach <z-ch@users.noreply.github.com>
2015-04-07 18:53:33 -0400

    Added no arg constructor
    Commented out the final too. Maybe bring it back later?

--- Player.java
+++ Player.java
@@ -7,21 +7,25 @@ import java.util.ArrayList;
  */
 public class Player
 {
-    public final String color, name;
+    public /*final*/ String color, name;
     protected int cars, tokens;
     protected ArrayList<DestinationCard> destCards;
     protected ArrayList<TrainCarCard> trainCards;
-    
+
+    public Player() {
+        cars = 40; tokens = 30; // fix this so name and color can be final
+        destCards = new ArrayList<>(); trainCards = new ArrayList<>();
+    }
     /**
      * Constructor for objects of type Player
      * @param n name of Player
-     * @param c color of Play
+     * @param c color of Player
      */
     public Player(String n, String c) {
-        cars = 40; tokens = 30;
+        this();
         color = c; name = n;
     }
-    
+
     /**
      * draws a destination card
      * @param d Deck of DestinationCards
@@ -31,7 +35,12 @@ public class Player
         DestinationCard c = (DestinationCard) d.drawCard();
         destCards.add(c);
     }
-    
+
+    public void addDestinationCard(Card c) {
+        
+        destCards.add((DestinationCard) c);
+    }
+
     /**
      * draws a train card
      * @param d Deck of TrainCarCards
@@ -41,10 +50,9 @@ public class Player
         TrainCarCard c = (TrainCarCard) d.drawCard();
         trainCards.add(c);
     }
-    
-    public void drawTrainCarCard(ArrayList<TrainCarCard> river, int clickedCard) {
+
+    public void addTrainCarCard(Card c) {
         // maybe add a instanceof check?
-        TrainCarCard c = river.remove(clickedCard);
-        trainCards.add(c);
+        trainCards.add((TrainCarCard) c);
     }
 }
