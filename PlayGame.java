58846b3f78ee01532179b1ead79b82e162c96b63
Zach <z-ch@users.noreply.github.com>
2015-04-11 16:42:35 -0400

    Commented out some stuff so it compiles for now

--- PlayGame.java
+++ PlayGame.java
@@ -35,7 +35,7 @@ implements MouseListener
                 "GREEN", "YELLOW", "RED", "BLUE", "BLACK"));
     protected Player[] playerList;
     int currPlayer = 0;
-    
+
     /**
      * Create all the TrainCarCards
      */
@@ -150,11 +150,11 @@ implements MouseListener
     public void mouseReleased( MouseEvent e ) { }
 
     public void mouseClicked( MouseEvent e ) {
-        clickX = e.getX();
-        clickY = e.getY();
-        click = true;
-        repaint();
-        e.consume();
+        //         clickX = e.getX();
+        //         clickY = e.getY();
+        //         click = true;
+        //         repaint();
+        //         e.consume();
     }
 
     /**
@@ -313,64 +313,59 @@ implements MouseListener
 
         catch (Exception e) {JOptionPane.showConfirmDialog(this, e.toString()); }
     }
-    
-//80 chars**********************************************************************
+
+    //80 chars**********************************************************************
     /**
      * Allows the currentPlayer to take their turn. Once gameplay starts, this 
      * will run on consecutive players until someone runs out of train tokens.
      * 
      * @param currentPlayer the player who's turn it is
      */
-    private void playerTurn(Player currentPlayer) {
-        boolean trainDeckDraw, trainRiverDraw = false;
-        boolean destinationDeckDraw = false, purchaseRoute = false;
-        trainDeckDraw = true;
-        if (trainDeckDraw) 
-            for (int i=0; i<2; i++) 
-                currentPlayer.drawTrainCarCard(trainDeck);
-        if (trainRiverDraw) {
-            int clickedCard = 0; // 0-4 inclusive
-            if (gameBoard.river.get(clickedCard).getColor().equals("rainbow")) {
-                currentPlayer.drawTrainCarCard(gameBoard.river, clickedCard);
-                gameBoard.river.add(clickedCard, (TrainCarCard) /*gameBoard.*/trainDeck.drawCard());
-            }
-            else {
-                currentPlayer.drawTrainCarCard(gameBoard.river, clickedCard);
-                gameBoard.river.add(clickedCard, (TrainCarCard) /*gameBoard.*/trainDeck.drawCard());
-                repaint(); //??
-                clickedCard = 1; // clicks new card THEY CAN'T CLICK RAINBOWS NOW DO THAT IN GUI
-                currentPlayer.drawTrainCarCard(gameBoard.river, clickedCard);
-                gameBoard.river.add(clickedCard, (TrainCarCard) /*gameBoard.*/trainDeck.drawCard());   
-            }
-        }
-        else if (destinationDeckDraw) {
-            //do the draw method from setupgame but 4 and 1
-        }
-        else if (purchaseRoute) {
-            // somehow the GUI will get two cities
-            //start temp
-            City city1, city2; 
-            city1 = CityList.getCity("cityname");
-            city2 = CityList.getCity("othercity");
-            //end temp
-            if (graph.hasEdge(city1.getName(),city2.getName())) { // also make sure the edge isn't taken!
-                // draw the edge on the board
-                // add the edge to the "taken" edges
-                // know which player got the edge
-            }
-        }
-    }
+    
+//     private void playerTurn(Player currentPlayer) {
+//         boolean trainDeckDraw, trainRiverDraw = false;
+//         boolean destinationDeckDraw = false, purchaseRoute = false;
+//         trainDeckDraw = true;
+//         if (trainDeckDraw) 
+//             for (int i=0; i<2; i++) 
+//                 currentPlayer.drawTrainCarCard(trainDeck);
+//         if (trainRiverDraw) {
+//             int clickedCard = 0; // 0-4 inclusive
+//             if (gameBoard.river.get(clickedCard).getColor().equals("rainbow")) {
+//                 currentPlayer.drawTrainCarCard(gameBoard.river, clickedCard);
+//                 gameBoard.river.add(clickedCard, (TrainCarCard) /*gameBoard.*/trainDeck.drawCard());
+//             }
+//             else {
+//                 currentPlayer.drawTrainCarCard(gameBoard.river, clickedCard);
+//                 gameBoard.river.add(clickedCard, (TrainCarCard) /*gameBoard.*/trainDeck.drawCard());
+//                 repaint(); //??
+//                 clickedCard = 1; // clicks new card THEY CAN'T CLICK RAINBOWS NOW DO THAT IN GUI
+//                 currentPlayer.drawTrainCarCard(gameBoard.river, clickedCard);
+//                 gameBoard.river.add(clickedCard, (TrainCarCard) /*gameBoard.*/trainDeck.drawCard());   
+//             }
+//         }
+//         else if (destinationDeckDraw) {
+//             //do the draw method from setupgame but 4 and 1
+//         }
+//         else if (purchaseRoute) {
+//             // somehow the GUI will get two cities
+//             //start temp
+//             City city1, city2; 
+//             city1 = CityList.getCity("cityname");
+//             city2 = CityList.getCity("othercity");
+//             //end temp
+//             if (graph.hasEdge(city1.getName(),city2.getName())) { // also make sure the edge isn't taken!
+//                 // draw the edge on the board
+//                 // add the edge to the "taken" edges
+//                 // know which player got the edge
+//             }
+//         }
+//     }
 
-        
     // draw train cards from deck, or from river
-
     // draw destination cards
     // OR you can buy a route
 }
-
-
-
-
 
 
 
